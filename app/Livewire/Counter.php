e50067edff0d9109aa972b6a7e0c0d23c8eb3aba
kardi.ib <kardi.ib@gmail.com>
2024-11-22 22:11:45 +0700

    wire model live

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -8,16 +8,9 @@ class Counter extends Component
 {
     public int $count = 10;
 
-/*************  ✨ Codeium Command ⭐  *************/
-    /**
-     * Change the count to a fixed value of 3.
-     *
-     * @return int The updated count value.
-     */
-/******  fb63e73b-09ad-4ed7-a741-6aef9f8eaa5f  *******/
-    public function changeCount()
+    public function changeCount(int $number)
     {
-        $this->count = 3;
+        $this->count = $number;
     }
 
     public function render()
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -2,6 +2,7 @@
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
     <h1>Hello World</h1>
     <p>{{ $count }}</p>
-    <button wire:click="changeCount">Change Count</button>
+    <input type="number" wire:model.live="count"/>
+    <button wire:click="changeCount(8)">Change Count</button>
 
 </div>
